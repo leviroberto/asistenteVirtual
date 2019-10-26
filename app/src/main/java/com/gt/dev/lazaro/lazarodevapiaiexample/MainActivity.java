cbdca018dda02b3ac9b840f729d53b3fde7fb191
levivelasquez <l953187894@gmail.com>
2019-10-26 09:51:59 -0500

    asdsad
    asdsadsa

---
+++ app/src/main/java/com/gt/dev/lazaro/lazarodevapiaiexample/MainActivity.java
@@ -0,0 +1,108 @@
+package com.gt.dev.lazaro.lazarodevapiaiexample;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.gson.JsonElement;
+
+import java.util.Map;
+
+import ai.api.AIListener;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+
+public class MainActivity extends AppCompatActivity implements AIListener, View.OnClickListener {
+
+    private Button btnVoice;
+    private TextView tvResult;
+    private AIService aiService;
+    private static final int REQUEST_INTERNET = 200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnVoice = (Button) findViewById(R.id.btn_voice);
+        tvResult = (TextView) findViewById(R.id.tv_result_main);
+
+        validateOS();
+
+        final AIConfiguration config = new AIConfiguration("f22524ef305b487683101d264ee4cf18",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
+
+        btnVoice.setOnClickListener(this);
+    }
+
+    private void validateOS() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_INTERNET);
+        }
+    }
+
+    @Override
+    public void onResult(AIResponse response) {
+        Result result = response.getResult();
+
+        // Get parameters
+        String parameterString = "";
+        if (result.getParameters() != null && !result.getParameters().isEmpty()) {
+            for (final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()) {
+                parameterString += "(" + entry.getKey() + ", " + entry.getValue();
+            }
+        }
+
+        // show result in textview
+        tvResult.setText("Query: " + result.getResolvedQuery() +
+        "\nAction: " + result.getAction() +
+        "\nParameters: " + parameterString);
+    }
+
+    @Override
+    public void onError(AIError error) {
+        tvResult.setText(error.toString());
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_voice:
+                aiService.startListening();
+                break;
+        }
+    }
+}
